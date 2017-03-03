376c1385ce6a98a1f26c5adef20497007aa90b7d
Manuel Palma <manuelpalma@MacBook-Pro-di-Manuel.local>
2017-03-03 21:30:49 +0300

    research

---
+++ src/app/app.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class UniversalApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          UniversalApp for all environments
+        </Text>
+        <Text style={styles.instructions}>
+          Palma
+        </Text>
+        <Text style={styles.instructions}>
+          ohyeah
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
